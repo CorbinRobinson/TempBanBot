44dd78c1918167aba9ba7ab16a4ad3855692d693
CorbinRobinson <corbinrobinson14@gmail.com>
2020-02-20 20:51:52 -0700

    -added uban command -added ban on mention -added perms set on join -added new bot login message -added broken command list (\n doesnt work)

--- bot.py
+++ bot.py
@@ -6,6 +6,10 @@ client = commands.Bot(command_prefix = '.')
 @client.event
 async def on_ready():
     print('Bot is ready')
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
     return await client.change_presence(activity = discord.Activity(type = 1, name = 'the fall of the bourgeoisie', url = 'https://twitch.tv/xQcOW'))
 
 async def is_me(ctx):
@@ -15,29 +19,75 @@ async def is_me(ctx):
 async def on_message(message):
     if message.author == client.user:
         return
+    # 191617352083832832 my id
+    # 202504795582365697 bo id
+    # 679444918757752883 scooby id
+    # 666838418134925347 lab channel id
+    if message.author.id == 202504795582365697:
+        bWords = [
+            "Scubarooni",
+            "scubarooni",
+            "Scuba",
+            "scuba",
+            "Corbin",
+            "corbin"
+        ]
+        mentionedMe = False
+        for user in message.mentions:
+            if user.id == 191617352083832832:
+                mentionedMe = True
 
-    if message.channel.id == 666838418134925347 and message.author.name == 'dodomino14':
-        await message.channel.send('Silence, prisoner.')
-        await message.delete(delay=0.0)
+        if message.content in bWords or mentionedMe:
+            response = [
+                "Silence, prisoner.",
+                "Keep it down, hoodlum.",
+                "ಠ_ಠ",
+                "You have no rights, scum.",
+                "Time to get the gag ( ͡° ͜ʖ ͡°)"
+            ]
+            await message.delete(delay=0.0)
+            await message.channel.send(random.choice(response))
+            await message.channel.send(f'Banned {message.author.mention}')
+            await message.author.ban(reason = "Bein a dumb bitch")
+
 
     await client.process_commands(message)
 
-
-'''
 @client.event
-async def on_message(self, message):
-    print('Message from {0.author}: {0.content}'.formate(message))
+async def on_member_join(member):
+    if member.id == 202504795582365697:
+        for c in member.guild.channels:
+            await c.set_permissions(member, send_tts_messages = False, embed_links = False, attach_files = False, mention_everyone = False)
+            if c.id == 633369722557300774:
+                await c.set_permissions(member, embed_links = True, attach_files = True)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def list(ctx):
+     cmds = [
+         "  GENERAL COMMANDS \n",
+         "-------------------- \n",
+         "8ball - ask a question, get an answer \n",
+         "ping - pongn \n",
+         "   ADMIN COMMANDS \n",
+         "-------------------- \n",
+         "stop - turns bot off \n",
+         "unban - unbans user#number \n"
+     ]
+     await ctx.channel.send(cmds)
+     return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@client.command()
+@commands.check(is_me)
+async def unban(ctx, *, member):
+    banned = await ctx.guild.bans()
+    member_name, member_discriminator = member.split('#')
+    for entry in banned:
+        user = entry.user
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.channel.send(f'Unbanned {user.mention}')
+            return
 
-    await client.process_commands(message)
-'''
 @client.command()
 async def ping(ctx):
     print('pong')
@@ -66,20 +116,11 @@ async def _8ball(ctx, *, question):
             "My sources say no.",
             "Outlook not so good.",
             "Very doubtful."]
-    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+    await ctx.send(f'Answer: {random.choice(responses)}')
 
 @client.command()
 @commands.check(is_me)
 async def stop(ctx):
     await client.close()
 
-
-'''
-@client.command()
-async def clear(ctx, amount=1):
-    await ctx.channel.purge(limit = amount)
-'''
-
-
-
-client.run('NjY2Njk1MzU4ODIyNDgxOTQw.Xh6Suw.ouIfrcTDmrBuWY-L8LBEAUl_d1g')
+client.run('NjY2Njk1MzU4ODIyNDgxOTQw.Xj5nyg.elZ1_pvj1fXgGwghUUQsleioD7w')
