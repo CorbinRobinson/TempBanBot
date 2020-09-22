384ff6ca2261e3a538616f048d90918978caa536
Corbin Robinson <59706109+CorbinRobinson@users.noreply.github.com>
2020-09-22 13:51:34 -0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -19,59 +19,44 @@ async def is_me(ctx):
 async def on_message(message):
     if message.author == client.user:
         return
-    # 191617352083832832 my id
     # 202504795582365697 bo id
-    # 679444918757752883 scooby id
-    # 666838418134925347 lab channel id
-    if message.author.id == 202504795582365697:
+    if message.channel.id == 666838418134925347 and message.author.id == 679444918757752883:
         bWords = [
-            "Scubarooni",
-            "scubarooni",
-            "Scuba",
-            "scuba",
-            "Corbin",
-            "corbin"
+        "Scubarooni",
+        "scubarooni",
+        "Scuba",
+        "scuba",
+        "Corbin",
+        "corbin"
         ]
-        mentionedMe = False
-        for user in message.mentions:
-            if user.id == 191617352083832832:
-                mentionedMe = True
 
-        if message.content in bWords or mentionedMe:
+        if message.content in bWords or 191617352083832832 in message.mentions:
             response = [
-                "Silence, prisoner.",
-                "Keep it down, hoodlum.",
-                "ಠ_ಠ",
-                "You have no rights, scum.",
-                "Time to get the gag ( ͡° ͜ʖ ͡°)"
+            "Silence, prisoner.",
+            "Keep it down, hoodlum.",
+            "ಠ_ಠ",
+            "You have no rights, scum.",
+            "Time to get the gag ( ͡° ͜ʖ ͡°)"
             ]
-            await message.delete(delay=0.0)
-            await message.channel.send(random.choice(response))
+            #await message.delete(delay=0.0)
+            #await message.channel.send(random.choice(response))
             await message.channel.send(f'Banned {message.author.mention}')
             await message.author.ban(reason = "Bein a dumb bitch")
 
 
     await client.process_commands(message)
 
-@client.event
-async def on_member_join(member):
-    if member.id == 202504795582365697:
-        for c in member.guild.channels:
-            await c.set_permissions(member, send_tts_messages = False, embed_links = False, attach_files = False, mention_everyone = False)
-            if c.id == 633369722557300774:
-                await c.set_permissions(member, embed_links = True, attach_files = True)
-
 @client.command()
 async def list(ctx):
      cmds = [
-         "  GENERAL COMMANDS \n",
-         "-------------------- \n",
-         "8ball - ask a question, get an answer \n",
-         "ping - pongn \n",
-         "   ADMIN COMMANDS \n",
-         "-------------------- \n",
-         "stop - turns bot off \n",
-         "unban - unbans user#number \n"
+     "  GENERAL COMMANDS\n",
+     "-------------------\n",
+     "8ball - ask a question, get an answer\n",
+     "ping - pongn\n",
+     "  ADMIN COMMANDS\n",
+     "-------------------\n",
+     "stop - turns bot off\n",
+     "unban - unbans user#number\n"
      ]
      await ctx.channel.send(cmds)
      return
@@ -123,4 +108,4 @@ async def _8ball(ctx, *, question):
 async def stop(ctx):
     await client.close()
 
-client.run('NjY2Njk1MzU4ODIyNDgxOTQw.Xj5nyg.elZ1_pvj1fXgGwghUUQsleioD7w')
+client.run('')
